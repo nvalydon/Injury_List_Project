15fdb797246aefeb3c2c2986f15c1255ab444a24
nvalydon <neeven.valydon@googlemail.com>
2020-01-13 13:50:43 +0000

    Updated the static folder with my HTML stuff

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,14 +1,65 @@
-     function sendPlayerData(){
-        let playerInfo = {
+function getValue() {
+    let checks = document.getElementsByClassName('posn');
+    arr = [];
+    let posn_id = {};
+    let str = '';
+    for ( i = 0; i < 15; i++) {
+        if ( checks[i].checked === true ) {
+
+            str = checks[i].value;
+            str = parseInt(str,10);
+            let position_data =     
+            {
+                id: str
+            }
+        
+        arr.push(position_data);    
+        }
+                        
+        }
+   
+
+    console.log(arr)
+}
+
+
+function sendPlayerData(){
+    let checks = document.getElementsByClassName('posn');
+    arr = [];
+    let posn_id = {};
+    let str = '';
+    for ( i = 0; i < 15; i++) {
+        if ( checks[i].checked === true ) {
+
+            str = checks[i].value;
+            str = parseInt(str,10);
+            let position_data =     
+            {
+                id: str
+            }
+        
+        arr.push(position_data);    
+        }
+                        
+        }
+   
+
+    console.log(arr)
+
+   
+    let playerInfo = {
 
             firstName: document.getElementById('firstName').value,
             lastName: document.getElementById('lastName').value,
             age: document.getElementById('age').value,
             typeOfInjury: document.getElementById('typeOfInjury').value,
             lengthOfInjury: document.getElementById('lengthOfInjury').value,
-            timePeriod: document.getElementById('timePeriod').value
+            timePeriod: document.getElementById('timePeriod').value,
+            positions: arr
+            
     
         };
+        
         JSON.stringify(playerInfo);
         axios.post('http://localhost:8080/player/create', playerInfo)
         .then(function (response){ 
@@ -22,18 +73,174 @@
 
 
      })
+     
     }     
 
-function sendPositionData(){
+// function sendPositionData(){
+//     let checks = document.getElementsByClassName('posn');
+//     let str = '';
+//     for ( i = 0; i < 15; i++) {
+//         if ( checks[i].checked === true ) {
+//             str += checks[i].value + " ";
+//         }
+//     }
 
-    let positionInfo = {
-        position: document.getElementById('position').value
-    };
-    JSON.stringify(positionInfo);
-    axios.post('http://localhost:8080/position/create',positionInfo)
+//     let positionInfo = {
+//         position: str
+//     };
+//     JSON.stringify(positionInfo);
+//     axios.post('http://localhost:8080/position/create',positionInfo)
+//     .then(function (response){ 
+
+//         console.log(response);
+    
+//     })
+//     .catch(function (error){
+
+//         console.log(error);
+
+
+//  })
+ 
+// }
+
+function validateForm(){
+    let hasNumber = /\d/;
+    let fn = document.playerInputData.firstName.value;
+    let ln = document.playerInputData.lastName.value;
+    let age = document.playerInputData.age.value;
+    // let posn = document.playerInputData.position.value;
+    let DoI = document.playerInputData.typeOfInjury.value;
+    let LoI = document.playerInputData.lengthOfInjury.value;
+    let TP = document.playerInputData.timePeriod.value;
+    
+    //First Name Validation
+    if(fn == "") {
+        alert( "Please Enter a First Name" );
+        document.playerInputData.firstName.focus();
+        document.getElementById('firstName').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    if( hasNumber.test(fn)) {
+         alert( "Error - Name Cannot Contain Numbers" );
+         document.playerInputData.firstName.focus() ;
+         document.getElementById('firstName').style.backgroundColor = "#f54c4c";
+         return false;
+    }
+    if( fn.length < 2 ) {
+        alert( "First Name Too Short" );
+        document.playerInputData.firstName.focus();
+        document.getElementById('firstName').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    if( fn.length > 200 ) {
+        alert( "First Name Too Long" );
+        document.playerInputData.firstName.focus();
+        document.getElementById('firstName').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    // if(fn){
+    //     alert( "Name cannot contain Special Characters" );
+    //     document.playerInputData.firstName.focus();
+    //     document.getElementById('firstName').style.backgroundColor = "#f54c4c";
+    //     return false;
+    // }
+
+
+    //Last Name Validations
+     if(  ln == "" ) {
+        alert( "Please Enter a Last Name" );
+        document.playerInputData.lastName.focus();
+        document.getElementById('lastName').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    if( hasNumber.test(ln)) {
+         alert( "Error - Last Name cannot contain numbers" );
+         document.playerInputData.lastName.focus() ;
+         document.getElementById('lastName').style.backgroundColor = "#f54c4c";
+         return false;
+    }
+     if( ln.length < 2 ) {
+        alert( "Last Name Too Short" );
+        document.playerInputData.lastName.focus();
+        document.getElementById('lastName').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+     if( ln.length > 200 ) {
+        alert( "Last Name Too Long" );
+        document.playerInputData.lastName.focus();
+        document.getElementById('lastName').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    
+    //Age Validations
+    if(age == "" ) {
+        alert( "Please Enter an Age" );
+        document.playerInputData.age.focus();
+        document.getElementById('age').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    if(age < 1 || age > 120){
+        alert( "Age Must be Greater than 1 and less than 120" );
+        document.playerInputData.age.focus();
+        document.getElementById('age').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+
+   
+     //Description of Injury Validation
+     if(DoI == "") {
+        alert( "Please Enter a Description of the Injury" );
+        document.playerInputData.typeOfInjury.focus();
+        document.getElementById('typeOfInjury').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+
+     if( hasNumber.test(DoI)) {
+        alert( "Error - Description cannot contain numbers" );
+        document.playerInputData.typeOfInjury.focus();
+        document.getElementById('typeOfInjury').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+
+    if(DoI.length ==1 || DoI.length >= 150 ){
+        alert( "Error - Description must be between 1 and 150 Characters" );
+        document.playerInputData.typeOfInjury.focus();
+        document.getElementById('typeOfInjury').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+
+    //Length Of Injury Validation
+    if(LoI == "" ) {
+        alert( "Please Enter a Length Of Injury" );
+        document.playerInputData.lengthOfInjury.focus();
+        document.getElementById('lengthOfInjury').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    if(LoI < 1){
+        alert( "Length of Injury Must be Greater than 1" );
+        document.playerInputData.lengthOfInjury.focus();
+        document.getElementById('lengthOfInjury').style.backgroundColor = "#f54c4c";
+        return false;
+    }
+    if(TP == "Choose a Time Period"){
+        alert( "How long will the player be out for?" );
+        document.playerInputData.timePeriod.focus();
+        return false;
+    }
+    
+ 
+
+
+}
+
+function getPlayerData(){
+
+    pData = axios.get('http://localhost:8080/player/getAll')
     .then(function (response){ 
 
         console.log(response);
+        addDataToTable(response.data)
     
     })
     .catch(function (error){
@@ -41,8 +248,50 @@ function sendPositionData(){
         console.log(error);
 
 
- })
-}     
+ });
+}
+const table = document.getElementById("playerTable");
+function addDataToTable(dataToAdd){
+    for(let playerData of dataToAdd){
+
+        let row = document.createElement("tr");
+        let firstNameEntry = document.createElement("td")
+        firstNameEntry.innerHTML = playerData.firstName;
+        row.appendChild(firstNameEntry);
+
+        let lastNameEntry = document.createElement("td");
+        lastNameEntry.innerHTML = playerData.lastName;
+        row.appendChild(lastNameEntry);
+
+        let ageEntry = document.createElement("td");
+        ageEntry.innerHTML = playerData.age;
+        row.appendChild(ageEntry);
+
+        // let positionEntry = document.createElement("td");
+        // for(let i = 0; i< playerData.positions.length; i++ ){
+
+        //     playerData.positions.split(",",3);
+        //     positionEntry.innerHTML = playerData.positions + "/"
+        // }
+        // row.append(positionEntry);
+
+        let typeOfInjuryEntry = document.createElement("td");
+        typeOfInjuryEntry.innerHTML = playerData.typeOfInjury;
+        row.append(typeOfInjuryEntry);
+
+        let lengthOfInjuryEntry = document.createElement("td");
+        lengthOfInjuryEntry.innerHTML = playerData.lengthOfInjury + " " +playerData.timePeriod;
+        row.append(lengthOfInjuryEntry);
+
+        table.appendChild(row);
+        
+
+
+
+
+
+        
+    }
 
 
 
@@ -55,42 +304,7 @@ function sendPositionData(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
