6ab0818617b93f660b5ea38c9bea3e3b4ffa8afd
nvalydon <58259202+nvalydon@users.noreply.github.com>
2020-01-30 12:54:08 +0000

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO Positions (position) VALUES
+INSERT INTO position (position) VALUES
 ('CF'),('ST'),('RW'),('LW'),('RM'),('LM'),('CM'),('CAM'),
 ('CDM'),('CB'),('LB'),('RB'),('LWB'),('RWB'),('GK');
